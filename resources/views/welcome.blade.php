b4bda27b1905ef75ad7f65c9393817a84cc5f78a
TechGuy <dev.techguy@gmail.com>
2019-01-31 17:29:14 +0300

    Sample of resource rest api.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,109 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+	<title>Laravel</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+	<!-- Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+	<!-- Styles -->
+	<style>
+		html, body {
+			background-color: #fff;
+			color: #636b6f;
+			font-family: 'Raleway', sans-serif;
+			font-weight: 100;
+			height: 100vh;
+			margin: 0;
+		}
 
-            .full-height {
-                height: 100vh;
-            }
+		.full-height {
+			height: 100vh;
+		}
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+		.flex-center {
+			align-items: center;
+			display: flex;
+			justify-content: center;
+		}
 
-            .position-ref {
-                position: relative;
-            }
+		.position-ref {
+			position: relative;
+		}
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+		.top-right {
+			position: absolute;
+			right: 10px;
+			top: 18px;
+		}
 
-            .content {
-                text-align: center;
-            }
+		.content {
+			text-align: center;
+		}
 
-            .title {
-                font-size: 84px;
-            }
+		.title {
+			font-size: 84px;
+		}
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+		.message {
+			font-size: 20px;
+		}
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
+		.links > a {
+			color: #636b6f;
+			padding: 0 25px;
+			font-size: 12px;
+			font-weight: 600;
+			letter-spacing: .1rem;
+			text-decoration: none;
+			text-transform: uppercase;
+		}
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+		.m-b-md {
+			margin-bottom: 30px;
+		}
+	</style>
+</head>
+<body>
+<div class="flex-center position-ref full-height">
+	@if (Route::has('login'))
+		<div class="top-right links">
+			@auth
+				<a href="{{ url('/home') }}">Home</a>
+			@else
+				<a href="{{ route('login') }}">Login</a>
+				<a href="{{ route('register') }}">Register</a>
+			@endauth
+		</div>
+	@endif
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
+	<div class="content">
+		<div class="title m-b-md">
+			LARAVEL REST/API AND VUE JS
+		</div>
+
+		<div class="message m-b-md">
+			This rest api uses laravel resources, also you may like to only have <strong>REST/API</strong> Code or
+			<strong>REST/API & VUE JS</strong> Code, just on the link below.
+		</div>
+
+		<div class="links">
+			<br>
+			<a href="https://forge.laravel.com">Rest/API</a>
+			<a href="https://github.com/laravel/laravel">Rest/API & Vue js</a>
+		</div>
+
+		<div class="message m-b-md">
+			<br>
+			<br>
+			<br>
+			Learn practise and share <br><strong>O_o</strong>
+		</div>
+	</div>
+</div>
+</body>
 </html>
